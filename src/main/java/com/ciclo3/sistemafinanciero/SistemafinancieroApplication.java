9b2c89017984e9c0e36d7c52e258ccc1e1ab497a
paula.munoz <paula.munoz@ericsson.com>
2022-08-25 12:26:08 -0500

    V1

---
+++ src/main/java/com/ciclo3/sistemafinanciero/SistemafinancieroApplication.java
@@ -0,0 +1,13 @@
+package com.ciclo3.sistemafinanciero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemafinancieroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemafinancieroApplication.class, args);
+    }
+
+}
