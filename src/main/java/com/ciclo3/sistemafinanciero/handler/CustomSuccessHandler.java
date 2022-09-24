8e9aed70490d6244f14a42b2c5fdaaf0ba94db10
emartinezb <estefa0223@gmail.com>
2022-09-23 19:22:27 -0500

    Creación de Página con categorias

--- src/main/java/com/ciclo3/sistemafinanciero/handler/CustomSuccessHandler.java
+++ src/main/java/com/ciclo3/sistemafinanciero/handler/CustomSuccessHandler.java
@@ -41,7 +41,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         }
 
         if (esAdministrativo(roles)) {
-            url = "/VerEmpresas";
+            url = "/index";
         } else if (esOperativo(roles)) {
             url = "/VerMovimientos";
         } else {
